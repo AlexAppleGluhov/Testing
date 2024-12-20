c1976df65095edf7a766bb1a500c01b1c7095b31
Aleksandr Glukhov <k.korelskaya96@gmail.com>
2024-12-20 15:33:02 +0300

    Задание Л14 Junit5

---
+++ src/main/java/Lesson_14/Main.java
@@ -0,0 +1,9 @@
+package Lesson_14;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 31; i++) {
+            System.out.println(i + "!=" + Factorial.get(i));
+        }
+    }
+}
