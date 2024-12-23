013d6fd068372fefb66ef95c643e63e079cee0fd
Aleksandr Glukhov <k.korelskaya96@gmail.com>
2024-12-23 10:34:52 +0300

    Задание Л15

--- src/main/java/Lesson_14/Main.java
+++
@@ -1,9 +0,0 @@
-package Lesson_14;
-
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 0; i < 31; i++) {
-            System.out.println(i + "!=" + Factorial.get(i));
-        }
-    }
-}
